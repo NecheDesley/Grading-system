60bae80495068994374ab507a84b072b73669684
DESLEY <samdesley@gmail.com>
2022-08-18 00:50:45 +0100

    adjustments

--- assets/grade.js
+++ assets/grade.js
@@ -1,67 +1,138 @@
+
+
 const app = "==============\nWelcome to IMSU grading system with Javascript\n==============\n\n"
 
-const name = prompt(app + ' Please enter your fullname')
-if (name== null){
-    alert('Please input your name')
 
 
+var fullname = prompt(app + 'Enter your fullname')
 
+while (true) {
+    if (fullname == null) {
+        fullname = prompt('Something went wrong, Please provide your fullname')
+    }
+    else if (fullname == "") {
+        fullname = prompt('Please, this field is required enter your full name')
+    }
 
-    const level = prompt('please enter your class in senior secondary either SS1, SS2 OR SS3')
-
-
-    const English = prompt('Enter your English score')
-    const eng = Number.parseInt(English)
-    
-    const Maths = prompt('Enter your Maths score')
-    const mths = Number.parseInt(Maths)
-    
-    const Physics = prompt('Enter your Physics score')
-    const phy = Number.parseInt(Physics)
-    
-    const Chemistry = prompt('Enter your Chemistry score')
-    const chem = Number.parseInt(Chemistry)
-    
-    const Computer = prompt('Enter your Computer score')
-    const comp = Number.parseInt(Computer)
-    
-    
-    // for the average
-    const Average = (eng + mths + phy + chem + comp) / 5
-    
-    
-    // To get check your grade
-    if (Average >= 70) {
-        alert(name + ' in ' + level + '  Your grade is A and your avearge score is ' + Average)
-    }
-    
-    else if (Average >= 60) {
-        alert(name + ' in ' + level + '  Your grade is B and your avearge score is ' + Average)
-    }
-    
-    else if (Average >= 50) {
-        alert(name + ' in ' + level + '  Your grade is C and your avearge score is ' + Average)
-    }
-    
-    else if (Average >= 45) {
-        alert(name + ' in ' + level + '  Your grade is D and your avearge score is ' + Average)
-    }
-    
-    else if (Average >= 40) {
-        alert(name + ' in ' + level + '  Your grade is E and your avearge score is ' + Average)
-    }
-    
-    else if (Average <= 39) {
-        alert(name + ' in ' + level + '  Your grade is F and your avearge score is ' + Average)
-    }
-    
     else {
-        alert('Sorry, you do not have a score')
+        break
+
+    }
+}
+
+
+
+var studentClass = prompt('Enter your class')
+
+while (true) {
+    if (studentClass == null) {
+        studentClass = prompt('Please provide your class')
+    }
+    else if (studentClass == "") {
+        studentClass = prompt('Please, this field is required enter your class')
     }
 
+    else {
+        break
 
+    }
 }
 
-else{
-    alert("you've been logged out 🙄")
+
+
+
+
+const English = prompt('Enter your English score')
+const eng = Number.parseInt(English)
+while (true) {
+    if (eng == "" || English == null)
+        eng = prompt('please, your English score is required')
+    else {
+        break
+    }
 }
+
+
+const Maths = prompt('Enter your Maths score')
+const mths = Number.parseInt(Maths)
+while (true) {
+    if (mths == "" || mths == null)
+        mths = prompt('please, your English score is required')
+    else {
+        break
+    }
+}
+
+
+
+const Physics = prompt('Enter your Physics score')
+const phy = Number.parseInt(Physics)
+while (true) {
+    if (phy == "" || phy == null)
+        phy = prompt('please, your English score is required')
+    else {
+        break
+    }
+}
+
+
+
+const Chemistry = prompt('Enter your Chemistry score')
+const chem = Number.parseInt(Chemistry)
+while (true) {
+    if (chem == "" || chem == null)
+        chem = prompt('please, your English score is required')
+    else {
+        break
+    }
+}
+
+
+
+
+
+const Computer = prompt('Enter your Computer score')
+const comp = Number.parseInt(Computer)
+while (true) {
+    if (comp == "" || comp == null)
+        comp = prompt('please, your English score is required')
+    else {
+        break
+    }
+}
+
+
+
+
+// for the average
+const Average = (eng + mths + phy + chem + comp) / 5
+
+
+// To get check your grade
+if (Average >= 70) {
+    alert(fullname + ' in ' + studentClass + '  Your grade is A and your avearge score is ' + Average)
+}
+
+else if (Average >= 60) {
+    alert(fullname + ' in ' + studentClass + '  Your grade is B and your avearge score is ' + Average)
+}
+
+else if (Average >= 50) {
+    alert(fullname + ' in ' + studentClass + '  Your grade is C and your avearge score is ' + Average)
+}
+
+else if (Average >= 45) {
+    alert(username + ' in ' + studentClass + '  Your grade is D and your avearge score is ' + Average)
+}
+
+else if (Average >= 40) {
+    alert(fullname + ' in ' + studentClass + '  Your grade is E and your avearge score is ' + Average)
+}
+
+else if (Average <= 39) {
+    alert(fullname + ' in ' + studentClass + '  Your grade is F and your avearge score is ' + Average)
+}
+else {
+    alert('Sorry, you do not have a score')
+}
+
